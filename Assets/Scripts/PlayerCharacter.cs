dfa94b490624010f17eed49ce060bcb35a704e8e
alex <avrrussell@gmail.com>
2025-11-19 16:20:17 +0000

    Player can move with arrow keys

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerCharacter : MonoBehaviour
 {
+    private Vector3 _currentPosition;
+    private float _walkSpeed = 0.025f;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +15,23 @@ public class PlayerCharacter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Keyboard.current.upArrowKey.isPressed)
+        {
+            _currentPosition.y += _walkSpeed;
+        }
+        else if (Keyboard.current.downArrowKey.isPressed)
+        {
+            _currentPosition.y -= _walkSpeed;
+        }
+        else if (Keyboard.current.leftArrowKey.isPressed)
+        {
+            _currentPosition.x -= _walkSpeed;
+        }
+        else if (Keyboard.current.rightArrowKey.isPressed)
+        {
+            _currentPosition.x += _walkSpeed;
+        }
         
+        transform.position = _currentPosition;
     }
 }
