e04c603f0df75212371d27b601244f1ded515269
alex <avrrussell@gmail.com>
2025-11-19 16:25:41 +0000

    Player can sprint with shift

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -5,6 +5,7 @@ public class PlayerCharacter : MonoBehaviour
 {
     private Vector3 _currentPosition;
     private float _walkSpeed = 0.025f;
+    private float _runSpeed = 0.05f;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,21 +16,27 @@ public class PlayerCharacter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float moveSpeed = _walkSpeed;
+        if (Keyboard.current.shiftKey.isPressed)
+        {
+            moveSpeed = _runSpeed;
+        }
+
         if (Keyboard.current.upArrowKey.isPressed)
         {
-            _currentPosition.y += _walkSpeed;
+            _currentPosition.y += moveSpeed;
         }
         else if (Keyboard.current.downArrowKey.isPressed)
         {
-            _currentPosition.y -= _walkSpeed;
+            _currentPosition.y -= moveSpeed;
         }
         else if (Keyboard.current.leftArrowKey.isPressed)
         {
-            _currentPosition.x -= _walkSpeed;
+            _currentPosition.x -= moveSpeed;
         }
         else if (Keyboard.current.rightArrowKey.isPressed)
         {
-            _currentPosition.x += _walkSpeed;
+            _currentPosition.x += moveSpeed;
         }
         
         transform.position = _currentPosition;
